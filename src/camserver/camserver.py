060a4b5d7e83892bb656d363813cc118e27ee8b0
DByrne <byrne_daniel@hotmail.com>
2023-10-30 13:29:19 +0000

    testing on pi

--- src/camserver/camserver.py
+++ src/camserver/camserver.py
@@ -7,8 +7,17 @@ import atexit
 
 
 class Camera(object):
-    def __init__(self, camera_id=0):
-        self.cap = cv2.VideoCapture(0)
+    def __init__(self, camera_id=0, pi=False, width=640, height=480):
+        if pi:
+            self.pi = True
+            import picamera
+            self.cap = picamera.PiCamera()
+            self.cap.resolution = (640, 480)
+            self.cap.framerate = 24
+            self.rawCapture = picamera.array.PiRGBArray(self.cap, size=(640, 480))
+            self.stream = self.cap.capture_continuous(self.rawCapture, format="bgr", use_video_port=True)
+        else:
+            self.cap = cv2.VideoCapture(0)
 
         # just in case the camera was not released
         atexit.register(self.__del__)
@@ -17,9 +26,14 @@ class Camera(object):
         self.cap.release()
 
     def get_frame(self):
-        ret, frame = self.cap.read()
-        ret, jpeg = cv2.imencode('.jpg', frame)
-        return jpeg.tobytes()
+        if self.pi:
+            frame = self.stream.__next__().array
+            ret, jpeg = cv2.imencode('.jpg', frame)
+            return jpeg.tobytes()
+        else:
+            ret, frame = self.cap.read()
+            ret, jpeg = cv2.imencode('.jpg', frame)
+            return jpeg.tobytes()
 
 
 
@@ -29,10 +43,13 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Run the camera server.')
     parser.add_argument('--camera_id', type=int, default=0)
+    parser.add_argument('--pi', action='store_true')
+    parser.add_argument('--width', type=int, default=640)
+    parser.add_argument('--height', type=int, default=480)
     args = parser.parse_args()
 
     app = Flask(__name__)
-    cam = Camera(args.camera_id)
+    cam = Camera(camera_id=args.camera_id, pi=args.pi, width=args.width, height=args.height)
 
     width = int(cam.cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(cam.cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
